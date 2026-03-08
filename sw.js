a6fcbec7c248a72d49b0dc8ed314299d62308069
Michael Tong <michael_tong@outlook.com>
2026-03-08 15:26:05 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-332ae71be24598aedfaa.js"
+    "url": "webpack-runtime-39fc50ce41c18ab8d46e.js"
   },
   {
     "url": "framework-a3ff8b5ee67514b98b0b.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7bde92de4a151338e6d2130ff5cc3b75"
+    "revision": "b753d09d5f6075855fcfa04941892a8a"
   },
   {
     "url": "manifest.webmanifest",
